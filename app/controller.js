232ff262004ac96d4d68de652eb6100b5b1f3185
Mathieu Dubois <skiant.be@gmail.com>
2015-06-17 22:49:59 +0200

    Initial commit

---
+++ app/controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default appController;
+
+appController.$inject = ['$scope'];
+function appController ($scope) {
+	console.log('controller init');
+}
---
+++ app/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import angular from 'angular';
+import controller from './controller.js'
+
+export default angular.module('photobooth', [])
+.controller('photoboothController', controller);
