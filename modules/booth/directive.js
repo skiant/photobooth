cb94a05d3c57472270e96445de522be42cbd8742
Mathieu Dubois <skiant.be@gmail.com>
2015-06-25 22:09:03 +0200

    WIP capturing frames

--- modules/booth/directive.js
+++ modules/booth/directive.js
@@ -10,15 +10,58 @@ export default function () {
 		template: template,
 		scope: {
 			type: '='
-		}
+		},
+		link: bindVideoElement
 	}
 }
 
-webcamDirectiveController.$inject = ['$scope', 'webcamService'];
-function webcamDirectiveController ($scope, webcamService) {
-	var vm=this;
+webcamDirectiveController.$inject = ['$scope', 'webcamService', '$document', '$q'];
+function webcamDirectiveController ($scope, webcamService, $document, $q) {
+	let vm=this;
+	vm.result = null;
 	webcamService.stream.then(streamUrl => {
 		vm.stream = streamUrl;
 	});
+
+	vm.capture = function () {
+		switch(vm.type) {
+			case 'single':
+				vm.result = vm.getFrames(1);
+				break;
+		}
+	}
+
+	vm.getFrames = function (framesAmount, framesDelay) {
+		let canvas;
+		let context;
+		let frames = new Array(framesAmount);
+		let def = $q.defer();
+
+		// create canvas
+		canvas = $document[0].createElement('canvas');
+		canvas.width = 1280;
+		canvas.height = 720;
+
+		// prepare context
+		context = canvas.getContext('2d');
+
+		// capture each frame
+		frames.forEach((frame, index) => {
+			context.drawImage(vm.videoElement, 0, 0, canvas.width, canvas.height);
+			frames[index] = canvas.toDataURL('image/jpg');
+
+			if (index === frames.length-1) {
+				def.resolve(frames);
+			}
+		});
+
+		return def.promise;
+	}
+
 	return vm;
+}
+
+
+function bindVideoElement (scope, elem, attr) {
+	scope.booth.videoElement = elem.find('video')[0];
 }
