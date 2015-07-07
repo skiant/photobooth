13614dcd6a849df385cc494944e10424d6eb0bd6
Mathieu Dubois <skiant.be@gmail.com>
2015-07-08 00:59:53 +0200

    Remove class abstraction to avoid DI mess

--- client/modules/booth/webcamService.js
+++ client/modules/booth/webcamService.js
@@ -9,74 +9,72 @@ if (getUserMedia) {
 
 let interval;
 
-class webcamService {
-	constructor ($q, $document, $interval) {
-		var def = $q.defer()
+webcamService.$inject = ['$q', '$document', '$interval'];
+export default function webcamService($q, $document, $interval) {
+	var def = $q.defer()
 
+	let service = {
 		// prepare internal elements
-		this.timer = null;
-		this.video = $document[0].createElement('video');
-		this.video.setAttribute('autoplay', true);
-		this.canvas = $document[0].createElement('canvas');
-		this.canvas.width = 945;
-		this.canvas.height = 945;
-		this.$q = $q;
-		this.$interval = $interval;
+		timer: null,
+		video: $document[0].createElement('video'),
+		canvas: $document[0].createElement('canvas'),
+		stream: def.promise,
+		getFrames: getFrames,
+		startTimer: startTimer
+	}
 
-		this.stream = def.promise;
-
-		if (!getUserMedia) {
-			console.error('Not possible to find user media');
-			return false;
-		}
-
-		let constraints = {
-			audio: false,
-			video: {
-				mandatory: {
-					minWidth: 1280,
-					minHeight: 720
-				}
+	service.video.setAttribute('autoplay', true);
+	service.canvas.width = 945;
+	service.canvas.height = 945;
+	let constraints = {
+		audio: false,
+		video: {
+			mandatory: {
+				minWidth: 1280,
+				minHeight: 720
 			}
-		};
+		}
+	};
 
+	if (!getUserMedia) {
+		console.error('Not possible to find user media');
+		return false;
+	} else {
 		getUserMedia(
 			constraints,
 			stream => {
-				this.video.src = window.URL.createObjectURL(stream)
-				def.resolve(this.video.src);
+				service.video.src = window.URL.createObjectURL(stream)
+				def.resolve(service.video.src);
 			},
-			error => {
-				def.reject(error);
-			}
+			error => { def.reject(error); }
 		);
 	}
 
-	// Record frames
-	getFrames (framesAmount, framesDelay, captureStartDelay) {
+	// record frames
+	function getFrames (framesAmount, framesDelay, captureStartDelay) {
 		captureStartDelay = captureStartDelay || 5000;
-		let def = this.$q.defer();
+		let def = $q.defer();
 		let frames = new Array(framesAmount);
 		// prepare context
-		let context = this.canvas.getContext('2d');
+		let context = service.canvas.getContext('2d');
 
 		// capture each frame
 		frames.fill(0);
-		this.startTimer(captureStartDelay);
+		service.startTimer(captureStartDelay);
 		window.setTimeout(()=> {
 			frames.forEach((frame, index) => {
 				window.setTimeout(() => {
 					let newWidth = 1280 * (this.canvas.height / 720);
 					let posX = (newWidth - this.canvas.width) / 2;
 					context.drawImage(this.video, -367, 0, 1280 * (945/720), 945);
-					frames[index] = this.canvas.toDataURL('image/jpg');
+					frames[index] = service.canvas.toDataURL('image/jpg');
 
 					// end the capture with the last frame
 					if (index === frames.length-1) {
 						def.resolve(frames);
 					} else if (framesDelay > 1000){
 						// show the timer if it's bigger than 1 second
-						this.startTimer(framesDelay);
+						service.startTimer(framesDelay);
 					}
 				},
 				framesDelay * index + 1
@@ -90,17 +88,18 @@ class webcamService {
 		return def.promise;
 	}
 
-	startTimer (delay) {
-		this.$interval.cancel(interval);
-		this.timer = delay/1000;
-		interval = this.$interval(() => {}, 1000, this.timer);
+	// handles timer display
+	function startTimer (delay) {
+		$interval.cancel(interval);
+		service.timer = delay/1000;
+		interval = $interval(() => {}, 1000, service.timer);
 
 		interval.then(
-			()=>{ this.timer = null;},
+			()=>{ service.timer = null;},
 			null,
-			()=>{ if (this.timer > 0){ this.timer--; } }
+			()=>{ if (service.timer > 0){ service.timer--; } }
 		);
 	}
-}
-webcamService.$inject = ['$q', '$document', '$interval'];
-export default webcamService;
+
+	return service;
+};
